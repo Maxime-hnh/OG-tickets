707b7af319373ed0519b68c4659ecf842a8434e2
Maxime <maxime.huynh@okolo.fr>
2024-09-27 15:00:34 +0200

    add dotenv java and spring dotenv

--- server/src/main/java/com/studi/OG_tickets/security/SecurityConstants.java
+++ server/src/main/java/com/studi/OG_tickets/security/SecurityConstants.java
@@ -1,8 +1,6 @@
 package com.studi.OG_tickets.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.io.Encoders;
+import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
@@ -10,13 +8,10 @@ import java.util.Base64;
 
 public class SecurityConstants {
 
-
-
-//  private static final String SECRET_KEY_BASE64 = System.getenv("JWT_SECRET_KEY");
-  private static final String SECRET_KEY = "b+wqMmoy8sZOHgc+54v6Gg1D775/BE8itC8CgmHN/nYcoE4rtF678pShIUTXIvg57XU7tNlgZjtlTtfhwWTbDtE/qwhpnItLnwhHzQwtxg6W0age7FY/FPpwdsn9HbA3b+cWMpGbx/rCrXH4wCzq4M+5qCqpSdJSXDFrA08MInm3KlU1GQTeBtjNXSYntk9UdFGTYByUj3BEHuDBTSzV+V6UCFPhkUvr3svRSPH9NWYISWweR9OC5x6yEUaVGRb9bbXx01XrVxQZNpGMPlbVf7sGszZXi5sbWaUb5GpXKjFdaZv1yozHc8vAab94mkC/ewKhBROa6RbdsrbzXFCkTdgy/ZOpGjFFkMPC67KjYJk=";
+  private static final Dotenv dotenv = Dotenv.configure().directory("server").load();
+  private static final String SECRET_KEY = dotenv.get("JWT_SECRET_KEY");
 
   public static final SecretKey JWT_SECRET = Keys.hmacShaKeyFor(Base64.getDecoder().decode(SECRET_KEY));
-
 
 
 }
