ee232f928e963ce440dc4c3135175cab5d9e7a27
Maxime <maxime.huynh@okolo.fr>
2024-09-28 00:53:48 +0200

    ad log to SecurityConstants

--- server/src/main/java/com/studi/OG_tickets/security/SecurityConstants.java
+++ server/src/main/java/com/studi/OG_tickets/security/SecurityConstants.java
@@ -12,5 +12,13 @@ public class SecurityConstants {
   private static final Dotenv dotenv = Dotenv.load();
   private static final String SECRET_KEY = dotenv.get("JWT_SECRET_KEY");
 
+  static {
+    if (SECRET_KEY == null || SECRET_KEY.isEmpty()) {
+      System.err.println("⚠ JWT_SECRET_KEY is null or empty");
+    } else {
+      System.out.println("✅ JWT_SECRET_KEY loaded successfully: " + SECRET_KEY);
+    }
+  }
+
   public static final SecretKey JWT_SECRET = Keys.hmacShaKeyFor(Base64.getDecoder().decode(SECRET_KEY));
 }
